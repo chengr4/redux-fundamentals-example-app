546b8179f1453254dfad8c940a8547ba49939944
Eddie Cheng <fcheng@th-wildau.de>
2021-07-03 22:56:30 +0800

    add enhancer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,19 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import rootReducer from './rootReducer'
+import { sayHiOnDispatch, includeMeaningOfLife } from './exampleAddons/enhancers'
 
-const store = createStore(rootReducer)
+let preloadedState = {
 
-export default store
+}
+
+// compose function is used to merge multiple enhancers together.
+const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife);
+
+/* 
+Beside rootReducer and preloadedState
+createStore can also take one more argument, 
+which is used to customize the store's abilities and give it new powers.
+*/
+const store = createStore(rootReducer, preloadedState, composedEnhancer);
+
+export default store;
